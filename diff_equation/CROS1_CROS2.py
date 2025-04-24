f6d4387bef4ce7887672e69525dfbc3b970d644e
Vladislav <2007.5v.voskoboinik.vladislav@gmail.com>
2025-04-25 01:40:38 +0300

    CROS1_CROS2-fix

--- diff_equation/CROS1_CROS2.py
+++ diff_equation/CROS1_CROS2.py
@@ -6,7 +6,7 @@ t_0 = -1
 T = 2
 y_00 = 3
 eps = 0.02
-M = 500
+M = 100
 
 
 
@@ -35,18 +35,17 @@ for m in range(M) :
     u_CROS1[m + 1] = u_CROS1[m] + tau*w_1.real  
 
 
-a11 = 0.1j
-a21 = 0.8 - 1.3j
-a22 = 0.2 + 0.1j
-b1 = - 0.2j
-b2 = 1 - 0.9j
-c21 = 1 - 0.2j #before: c21 = 0.5 - 0.2j 
+a11 = 123123 #a11 = 0.1+1/30*11**0.5j
+a22 = 0.2+0.1j
+b1 =0.1941430241155180-0.2246898944678803j
+b2 = 0.8058569758844820-0.8870089521907592j
+c21 = 0.2554708972958462-0.2026195833570109j
+a21 = 0.5617645150714754-1.148223341045841j
 # CROS2
 for m in range(M) :
     w_1= f(u_CROS2[m], t[m])/(1 - a11 * tau *f(u_CROS2[m], t[m]))
     w_2= f(u_CROS2[m] + tau*(c21*w_1).real, t[m])/(1 - a22*tau*f_u(u_CROS2[m] + tau*(a21*w_1).real, t[m]))
     u_CROS2[m+1]=u_CROS2[m] + tau*(b1*w_1 + b2*w_2).real
-
 u_analytical = np.empty(M+1)
 u_analytical = eps / (1 - t)
 
